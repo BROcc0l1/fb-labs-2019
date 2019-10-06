fb9efc129452dad3c7ba98add481a56ba0c091b3
BROcc0l1 <xxxmakxxx777@gmail.com>
2019-10-07 02:00:12 +0300

    Unify frequency calculation for ngrams, add step. Fix entropy calculation for ngrams

--- cp_1/makovetskyi_fb-73_badarak_fb-73_cp1/lab1.py
+++ cp_1/makovetskyi_fb-73_badarak_fb-73_cp1/lab1.py
@@ -4,7 +4,7 @@ from math import log
 
 
 '''
-а б в г д е ж з и й к л м н о п р с т у ф х ц ч ш щ в ь э ю я
+а б в г д е ж з и й к л м н о п р с т у ф х ц ч ш щ ы ь э ю я
 
 31
 '''
@@ -25,40 +25,39 @@ def freq_by_number(numbers_dict, text_length):
 	return numbers_dict
 
 
-def calculate_freq_letters(text):
+def calculate_freq_ngrams(text, ngram_len=1, step=1):
+
+	# Step MUST be less or equal to ngram length and more than zero
+	# Ngram length must be greater than zero
+	# Probably fix this later
 
 	freq = {}
 
-	for letter in text:
-		if letter in freq:
-			freq[letter] += 1
+	for i in range(len(text)-ngram_len+1):
+		ngram = text[i : i+ngram_len : step]
+		if ngram in freq:
+			freq[ngram] += 1
 		else:
-			freq[letter] = 1
+			freq[ngram] = 1
 
 	return freq_by_number(freq, len(text))
 
 
-def calculate_freq_bigrams(text):
+def calculate_entropy(freq_dict, ngram_len=1):
 
-	freq = {}
-
-	for i in range(len(text)-1):
-		bigram = text[i:i+2]
-		if bigram in freq:
-			freq[bigram] += 1
-		else:
-			freq[bigram] = 1
-
-	return freq_by_number(freq, len(text))
-
-
-def calculate_entropy(freq_dict):
+	# Ngram length must be greater than zero
+	# Probably fix this later
 
 	entropy = 0
 	for i in freq_dict.values():
 		entropy += i * log(i, 2)
 
-	return -entropy
+	return -entropy / ngram_len
+
+
+def calculate_redundancy(entropy):
+
+	pass
 
 
 def print_frequencies(freq):
@@ -138,14 +137,14 @@ def main():
 
 	replaced_spaces_text = re.sub(' ', '_', data) # Replace spaces for better readability 
 
-	freq_letters = calculate_freq_letters(replaced_spaces_text)
-	freq_bigrams = calculate_freq_bigrams(replaced_spaces_text)
+	freq_letters = calculate_freq_ngrams(replaced_spaces_text)
+	freq_bigrams = calculate_freq_ngrams(replaced_spaces_text, 2)
 
 	#print_frequencies(freq_letters)
 	#print_frequencies(freq_bigrams)
 
 	entropy_letters = calculate_entropy(freq_letters)
-	entropy_bigrams = calculate_entropy(freq_bigrams)
+	entropy_bigrams = calculate_entropy(freq_bigrams, 2)
 
 	#print(entropy_letters, entropy_bigrams)
 
@@ -156,14 +155,14 @@ def main():
 
 	no_space_text = data_remove_spaces(data)
 
-	freq_letters_nospace = calculate_freq_letters(no_space_text)
-	freq_bigrams_nospace = calculate_freq_bigrams(no_space_text)
+	freq_letters_nospace = calculate_freq_ngrams(no_space_text)
+	freq_bigrams_nospace = calculate_freq_ngrams(no_space_text, 2)
 
 	#print_frequencies(freq_letters_nospace)
 	#print_frequencies(freq_bigrams_nospace)
 
 	entropy_letters_nospace = calculate_entropy(freq_letters_nospace)
-	entropy_bigrams_nospace = calculate_entropy(freq_bigrams_nospace)
+	entropy_bigrams_nospace = calculate_entropy(freq_bigrams_nospace, 2)
 
 	#print(entropy_letters_nospace, entropy_bigrams_nospace)
 
@@ -171,6 +170,8 @@ def main():
                         entropy_letters, entropy_bigrams,
                         freq_letters_nospace, freq_bigrams_nospace,
                         entropy_letters_nospace, entropy_bigrams_nospace)
+
+	print(len(replaced_spaces_text))
 
 
 
