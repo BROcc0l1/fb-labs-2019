0488ff408252c1edbf4716c16f641dfc6651adab
BROcc0l1 <xxxmakxxx777@gmail.com>
2019-11-11 11:16:57 +0200

    Update parser from cp_1

--- cp_1/makovetskyi_fb-73_badarak_fb-73_cp1/parser.py
+++ cp_1/makovetskyi_fb-73_badarak_fb-73_cp1/parser.py
@@ -1,8 +1,7 @@
 import os
 import re
-import sys
 
-def parse(filepath, keep_spaces=False):
+def parse(filepath):
 
 
 	raw_data = ''
@@ -15,16 +14,13 @@ def parse(filepath, keep_spaces=False):
 
 		raw_data = start_file.read()
 	
-	if keep_spaces:
-		raw_data = ' '.join(raw_data.split())			# Substitute whitespace characters with spaces
+
+	raw_data = ' '.join(raw_data.split())			# Substitute whitespace characters with spaces
 
 	for symbol in raw_data:
 
 		symbol = symbol.lower()
-		if keep_spaces:
-			matched = re.match(r'^[а-я ]$', symbol) 	# Only keep russian symbols and spaces
-		else:
-			matched = re.match(r'^[а-я]$', symbol)		# Only keep russian symblos
+		matched = re.match(r'^[а-я]$', symbol) 		# Only keep russian symbols
 
 		if matched:
 
@@ -38,8 +34,7 @@ def parse(filepath, keep_spaces=False):
 
 			parsed_data += symbol
 
-	if keep_spaces:
-		parsed_data = ' '.join(parsed_data.split())		# Substitute whitespace characters after filtering
+	parsed_data = ' '.join(parsed_data.split())		# Substitute whitespace characters after filtering
 
 
 	with open(parsed_file_path, 'w', encoding='utf8') as parsed_file:
@@ -47,23 +42,8 @@ def parse(filepath, keep_spaces=False):
 		parsed_file.write(parsed_data)
 
 
-	print('Data parsed to \'' + parsed_file_path + '\'')	
+	print('Text parsed to \'' + parsed_file_path + '\'')	
 
 
 
-
-def main():
-
-	filepath = sys.argv[1]
-	keep_spaces = False
-
-	
-	if len(sys.argv) > 2:
-		if sys.argv[2] == '-ks':
-			keep_spaces = True
-	
-
-	parse(filepath, keep_spaces)
-
-
-main()
+parse('text.txt')
