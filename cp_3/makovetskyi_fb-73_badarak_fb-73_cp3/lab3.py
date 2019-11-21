d1b21f7359dfd3f851462bef37053e1b22694759
BROcc0l1 <xxxmakxxx777@gmail.com>
2019-11-21 20:41:04 +0200

    fix int to bigram, start checking text reality, start solving linear eq

--- cp_3/makovetskyi_fb-73_badarak_fb-73_cp3/lab3.py
+++ cp_3/makovetskyi_fb-73_badarak_fb-73_cp3/lab3.py
@@ -12,12 +12,16 @@ ALPHABET_DICT = {
 
 THEORETICAL_MOST_FREQUENT_BIGRAMS = ['ст', 'но', 'то', 'на', 'ен']
 
-PRACTICAL_MOST_FREQUENT_BIGRAMS = {
-	'хб': 0.007740936653335054, 
-	'нк': 0.007224874209779383, 
-	'бй': 0.0068378273771126305, 
-	'юж': 0.006708811766223713, 
-	'шь': 0.006321764933556961}
+PRACTICAL_MOST_FREQUENT_BIGRAMS = ['нк', 'юж', 'хб', 'шь', 'мк']
+PRACTICAL_MOST_FREQUENT_BIGRAMS_DICT = {'нк': 0.014594735470419598, 'юж': 0.013552254365389628, 'хб': 0.012770393536617148, 'шь': 0.012770393536617148, 'мк': 0.012249152984102164}
+PR_WITH_NEWLINE = {'шь': 0.008515030318668558, 'мк': 0.008515030318668558, 'ьх': 0.007740936653335054, 'хб': 0.007224874209779383, 'нк': 0.006966842988001548}
+
+
+def import_data(filename):
+
+	with open(filename, 'r') as f:
+
+		return f.read()
 
 
 def bigram_to_int(bigram):
@@ -37,7 +41,10 @@ def int_to_bigram(number):
 
 	rev = {val: let for let, val in ALPHABET_DICT.items()}
 
-	pass
+	bigram_first = number // len(ALPHABET_DICT)
+	bigram_second = number % len(ALPHABET_DICT)
+
+	return rev[bigram_first] + rev[bigram_second]
 
 
 def gcd(a, b):
@@ -53,6 +60,8 @@ def modular_inverse(a, b):
 	x, y = 0, 1
 	u, v = 1, 0
 	m = b
+	a1 = a
+	b1 = b
 
 	while a != 0:
 
@@ -72,33 +81,128 @@ def modular_inverse(a, b):
 	if gcd == 1:
 		return x
 	else:
-		raise ValueError('Modular inverse for such values does not exist.')
+		raise ValueError('Modular inverse for such values does not exist:', a1, b1)
 
 
 # Solve linear equasion:
 # ax = b mod n
 def solve_linear_equasion(a, b, n):
+	#print(a, n, '<<<')
 
 	gcd_val = gcd(a, n)
 
 	if gcd_val == 1:
 
 		x = (b * modular_inverse(a, n)) % n
-		return list(x)
+
+		return [x]
 
 	elif gcd_val > 1:
 
-		if b % d != 0:
+		if b % gcd_val != 0:
 			raise ValueError('The equasion has no solutions.')
 
-		if b % d == 0:
+		if b % gcd_val == 0:
 
-			# d solutions
-			x0 = solve_linear_equasion(a / gcd_val, b / gcd_val, x / gcd_val)
-			res = []
-			for i in range(d):
-				res[i] = (n / gcd) * i + x0 % n
-
-			return res
+			pass
 
 
+def check_text_reality(text): # TODO: finish and improve
+
+	# Filtering using forbidden bigrams criterium
+
+	forbidden_bigrams = [
+		'аь', 'оь', 'уь', 'ыь', 'эь', 'иь', 'еь', 'яь', 'йь'
+		'аъ', 'оъ', 'уъ', 'ыъ', 'эъ', 'иъ', 'еъ', 'яъ', 'йъ'
+		'ъъ', 'ыы', 'ьь', 'йй']
+
+	for b in forbidden_bigrams:
+		if b in text:
+			return False
+
+
+	# Filtering using monogram frequencies
+
+	pass
+
+
+	# Filtering using bigram frequencies
+
+	pass
+	
+
+	return True
+
+
+def get_all_bigrams_pairs(arr):
+
+	res = []
+	print(type(arr))
+
+	# Find all possible practical pairs
+	for i in range(len(arr)):
+		# fix one of the items and add all others
+		for j in range(len(arr)):
+			# Do not pair a bigram with itself
+			if i != j:
+				res.append((arr[i], arr[j]))
+
+	return res
+
+
+def find_key(theor_bigram_pair, encr_bigram_pair):
+
+	# Y1 - Y2 = a(X1 - X2) (mod m**2)
+
+	global ALPHABET_DICT
+
+	m = len(ALPHABET_DICT)
+
+	theor_diff = (bigram_to_int(theor_bigram_pair[0]) - bigram_to_int(theor_bigram_pair[1])) % m
+	encr_diff = (bigram_to_int(encr_bigram_pair[0]) - bigram_to_int(encr_bigram_pair[1])) % m
+	#print(encr_diff, '######')
+	
+	le_solutions = solve_linear_equasion(encr_diff, theor_diff, m)
+
+	res = []
+
+	for a  in le_solutions:
+		a = a % m**2
+		b = (bigram_to_int(theor_bigram_pair[0]) - a * bigram_to_int(encr_bigram_pair[0])) % m**2
+		res += [a, b]
+
+	return res
+
+
+def attack_affine(theoretical, practical):
+
+	all_lang = get_all_bigrams_pairs(theoretical)
+	all_encr = get_all_bigrams_pairs(practical)
+
+	res = []
+
+	# Match all bigrams in language to the ones in ciphertext
+	# and find the keys for them
+	for i in range(len(all_lang)):
+		for j in range(len(all_encr)):
+
+				key = find_key(all_lang[i], all_encr[i])
+
+	print(key)
+
+
+
+
+def main():
+
+	global THEORETICAL_MOST_FREQUENT_BIGRAMS
+	global PRACTICAL_MOST_FREQUENT_BIGRAMS
+
+	plaintext = import_data('11.txt')
+
+	print(attack_affine(THEORETICAL_MOST_FREQUENT_BIGRAMS, PRACTICAL_MOST_FREQUENT_BIGRAMS))
+
+
+
+#print(get_all_bigrams_pairs(['аб','вг','де']))
+main()
