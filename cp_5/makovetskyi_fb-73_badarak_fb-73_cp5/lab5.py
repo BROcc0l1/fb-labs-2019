06dc5d9104cdc1b88cbb5723803fe7a7fd4db201
BROcc0l1 <xxxmakxxx777@gmail.com>
2019-12-24 02:46:49 +0200

    Finish random primes creation

--- cp_5/makovetskyi_fb-73_badarak_fb-73_cp5/lab5.py
+++ cp_5/makovetskyi_fb-73_badarak_fb-73_cp5/lab5.py
@@ -3,16 +3,17 @@ from math import gcd
 
 
 
-def prime_by_miller_rabin(p, rounds):
+def prime_by_miller_rabin(p, rounds=1024):
 
 	# simple prime division tests
-	primes = [2, 3, 5, 7, 13, 17, 19, 23, 27, 29, 31, 37]
+	primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 27, 29, 31, 37]
 
 	if p in primes or p == 1:
 		return True
 
 	for pr in primes:
 		if p % pr == 0:
+			print('div')
 			return False
 
 	d = p - 1
@@ -27,44 +28,54 @@ def prime_by_miller_rabin(p, rounds):
 		x = random.randrange(2, p)
 
 		if gcd(x, p) > 1:
+			print('T-1')
 			return False
 
 		x = pow(x, d, p)
 
-		if x in (1, -1):
-			return True
+		if x in (1, p - 1):
+			continue
+
+		xr = x
 
 		for r in range(1, s):
-			xr = pow(x, 2**r, p)
-			if xr == -1:
+			xr = pow(xr, 2, p)
+			if xr == p - 1:
 				return True
-			elif xr == 1:
+			if xr == 1:
+				print('T-2')
 				return False
 
+		if xr != p - 1:
+			return False
 
-		return True
 
-			
-#print(prime_by_miller_rabin(1299827, 128))
+	return True
 
-# TODO: finish this 
-def create_random_number(min, max):
 
-	x = random.randrange(min, max)
+def create_random_prime(min, max):
+
+	x = random.randrange(min, max + 1)
 	m0 = 0
 	if x % 2 == 0:
 		m0 = x + 1
 	else:
 		m0 = x
 
-	print(x, m0)
+	#print(x, m0)
 
 	for i in range((max - m0) // 2):
-
 		p = m0 + 2 * i
-		if prime_by_miller_rabin(p, 1024):
+		print(p)
+		if prime_by_miller_rabin(p):
 			return p
+		else:
+			return create_random_prime(min, max)
+
+
+
+
 		
 
-
-print(create_random_number(1, 10))
+#print(prime_by_miller_rabin(7571, 128))
+create_random_prime(1, 10000)
